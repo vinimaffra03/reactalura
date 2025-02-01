ac39cd6da35e10c4f0c7f26d6a7b9073d210d716
vinimaffra03 <viniciushmdem15@gmail.com>
2025-02-01 20:32:12 -0300

    fisrt commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Banner from './componentes/Banner'
+import CampoTexto from './componentes/CampoTexto'
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <Banner />
+        <CampoTexto />
+        <CampoTexto />
+        <CampoTexto />
+        <CampoTexto />
+      </div>
+    </>
+  )
+}
+
+export default App
