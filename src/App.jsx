77aaf184a21b2713a8e95722e2602877e3e3f538
vinimaffra03 <viniciushmdem15@gmail.com>
2025-02-02 22:42:38 -0300

    componentes props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import Banner from './componentes/Banner'
-import CampoTexto from './componentes/CampoTexto'
+import Formulario from './componentes/Formulario'
 
 function App() {
   return (
     <>
       <div className='App'>
         <Banner />
-        <CampoTexto />
-        <CampoTexto />
-        <CampoTexto />
-        <CampoTexto />
+        <Formulario />
       </div>
     </>
   )
---
+++ src/componentes/Formulario/index.jsx
@@ -0,0 +1,18 @@
+import CampoTexto from '../CampoTexto'
+import './Formulario.css'
+
+const Formulario = () => {
+    return(
+        <section className='formulario'>
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
+                <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <button>Criar Card</button>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
